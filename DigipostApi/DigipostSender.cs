ad3c8af3e38bd56540dedd6e46297893074d69b5
Erlend Oftedal <erlend@oftedal.no>
2011-09-01 08:46:49 +0200

    Upgrade for new version of API

--- DigipostApi/DigipostSender.cs
+++ DigipostApi/DigipostSender.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
@@ -11,8 +12,6 @@ using System.Xml.XPath;
 
 namespace DigipostApi
 {
-
-
     public class DigipostSender
     {
         private readonly bool _smsVarsling;
@@ -20,45 +19,68 @@ namespace DigipostApi
         private readonly int _avsenderId;
         private readonly XmlNamespaceManager _namespaceMgr;
         private readonly string _host;
+        private const String _digipostXml = "application/vnd.digipost-v1+xml";
+        private const String _namespace = "http://api.digipost.no/schema/v1";
+
         public DigipostSender(string host, X509Certificate2 sertifikat, int avsenderId, bool smsVarsling)
         {
             _smsVarsling = smsVarsling;
             _sertifikat = sertifikat;
             _avsenderId = avsenderId;
             _namespaceMgr = new XmlNamespaceManager(new NameTable());
-            _namespaceMgr.AddNamespace("dp", "http://www.digipost.no/xsd/avsender1_3");
+            _namespaceMgr.AddNamespace("dp", _namespace);
             _host = host;
         }
 
 
-        public bool Send(string forsendelsesId, string kundeId, string digipostAdresse, string emne, byte[] fil)
+        public bool Send(string forsendelsesId, string digipostAdresse, string emne, byte[] fil)
         {
-            var url = OpprettForsendelse(forsendelsesId, kundeId, digipostAdresse, emne);
+            var forsendelsesUrl = GetUrlForCreatingMessage();
+            var url = CreateMessage(forsendelsesUrl, forsendelsesId,digipostAdresse, emne);
             var request = (HttpWebRequest)WebRequest.Create(url);
-            SetHeaders(request, "application/octet-stream", fil);
+            SetHeaders("POST", request, "application/octet-stream", fil);
             using (var stream = request.GetRequestStream())
             {
                 stream.Write(fil, 0, fil.Length);
             }
-            var xml = PostAndGetXml(request);
-            return xml.XPathSelectElement("//dp:status[.='LEVERT']", _namespaceMgr) != null;
+            var xml = SendAndGetXml(request);
+            return xml.XPathSelectElement("//dp:status[.='DELIVERED']", _namespaceMgr) != null;
 
         }
-
-        private string OpprettForsendelse(string forsendelsesId, string kundeId, string digipostAdresse, string emne)
+        private string GetUrlForCreatingMessage()
         {
-            var request = (HttpWebRequest)WebRequest.Create(_host + "/forsendelse");
-            var forsendelse = GetXml(forsendelsesId, kundeId, digipostAdresse, emne);
+            var request = (HttpWebRequest)WebRequest.Create(_host + "/");
+            SetHeaders("GET", request, _digipostXml, new byte[0]);
+            var xml = SendAndGetXml(request);
+            return ExtractUriFromLink("/relations/create_message", xml);
+        }
+
+
+        private string CreateMessage(string forsendelsesUrl, string forsendelsesId, string digipostAdresse, string emne)
+        {
+            var request = (HttpWebRequest)WebRequest.Create(forsendelsesUrl);
+            var forsendelse = GetXml(forsendelsesId, digipostAdresse, emne);
             var body = Encoding.UTF8.GetBytes(forsendelse.Declaration + "\n" + forsendelse);
-            SetHeaders(request, "application/vnd.digipost+xml", body);
+            SetHeaders("POST", request, _digipostXml, body);
             using (var stream = request.GetRequestStream())
             {
                 stream.Write(body, 0, body.Length);
             }
-            var xml = PostAndGetXml(request);
-            return xml.XPathSelectElement("//dp:links/dp:rel[.='Legg til innhold']/../dp:uri", _namespaceMgr).Value;
+            var xml = SendAndGetXml(request);
+            return ExtractUriFromLink("/relations/add_content", xml);
         }
-        private XDocument PostAndGetXml(HttpWebRequest request)
+
+        private String ExtractUriFromLink(String relEndsWith, XDocument xml)
+        {
+            return xml
+                .XPathSelectElements("//dp:link/dp:rel", _namespaceMgr)
+                .Where(element => element.Value.EndsWith(relEndsWith))
+                .First()
+                .XPathSelectElement("../dp:uri", _namespaceMgr)
+                .Value;
+        }
+
+        private static XDocument SendAndGetXml(HttpWebRequest request)
         {
             try
             {
@@ -78,8 +100,7 @@ namespace DigipostApi
                     {
                         using (var reader = new StreamReader(response.GetResponseStream()))
                         {
-                            string s = reader.ReadToEnd();
-                            throw new Exception(s, we);
+                            throw new Exception(reader.ReadToEnd(), we);
                         }
                     }
                 }
@@ -89,44 +110,42 @@ namespace DigipostApi
 
 
 
-        private XDocument GetXml(string forsendelsesId, string kundeId, string digipostAdresse, string emne)
+        private XDocument GetXml(string forsendelsesId, string digipostAdresse, string emne)
         {
-            XNamespace xmlns = "http://www.digipost.no/xsd/avsender1_3";
+            XNamespace xmlns = _namespace;
             var doc = new XDocument(new XDeclaration("1.0", "UTF-8", "yes"),
-                new XElement(xmlns + "enkeltforsendelse",
-                        new XElement(xmlns + "forsendelseId", forsendelsesId),
-                        new XElement(xmlns + "emne", emne),
-                        new XElement(xmlns + "mottaker",
-                            new XElement(xmlns + "kunde-id", kundeId),
-                            new XElement(xmlns + "digipostadresse", digipostAdresse)
-                        ),
-                        new XElement(xmlns + "smsVarsling", _smsVarsling)
+                new XElement(xmlns + "message",
+                        new XElement(xmlns + "messageId", forsendelsesId),
+                        new XElement(xmlns + "subject", emne),
+                        new XElement(xmlns + "digipostAddress", digipostAdresse),
+                        new XElement(xmlns + "smsNotification", _smsVarsling)
                     )
                 );
             return doc;
         }
 
 
-        private void SetHeaders(HttpWebRequest request, string contentType, byte[] body)
+        private void SetHeaders(String method, HttpWebRequest request, string contentType, byte[] body)
         {
-            request.Method = "POST";
+            request.Method = method;
             request.ContentType = contentType;
+            request.Accept = _digipostXml;
             request.Headers.Add("X-Digipost-UserId", _avsenderId.ToString());
-            request.Headers.Add("X-Digipost-Date", DateTime.Now.AddHours(-2).ToString("r"));
+            request.Date = DateTime.Now;
             request.Headers.Add("Content-MD5", Convert.ToBase64String(MD5.Create().ComputeHash(body)));
             SignRequest(request, _sertifikat);
         }
-        private void SignRequest(HttpWebRequest request, X509Certificate2 cert)
+        private static void SignRequest(HttpWebRequest request, X509Certificate2 cert)
         {
-            String s = request.Method.ToUpper() + "\n" +
-                       request.RequestUri.AbsolutePath.ToLower() + "\n" +
-                       "content-md5: " + request.Headers["Content-MD5"] + "\n" +
-                       "x-digipost-date: " + request.Headers["X-Digipost-Date"] + "\n" +
-                       "x-digipost-userid: " + request.Headers["X-Digipost-UserId"] + "\n" +
-                       HttpUtility.UrlEncode(request.RequestUri.Query).ToLower() + "\n";
+            var s = request.Method.ToUpper() + "\n" +
+                    request.RequestUri.AbsolutePath.ToLower() + "\n" +
+                    "content-md5: " + request.Headers["Content-MD5"] + "\n" +
+                    "date: " + request.Date.ToUniversalTime().ToString("r") + "\n" +
+                    "x-digipost-userid: " + request.Headers["X-Digipost-UserId"] + "\n" +
+                    HttpUtility.UrlEncode(request.RequestUri.Query).ToLower() + "\n";
 
             var rsa = cert.PrivateKey as RSACryptoServiceProvider;
-            byte[] privateKeyBlob = rsa.ExportCspBlob(true);
+            var privateKeyBlob = rsa.ExportCspBlob(true);
             var rsa2 = new RSACryptoServiceProvider();
             rsa2.ImportCspBlob(privateKeyBlob);
 
@@ -135,7 +154,6 @@ namespace DigipostApi
             var signature = rsa2.SignHash(hash, CryptoConfig.MapNameToOID("SHA256"));
             request.Headers.Add("X-Digipost-Signature", Convert.ToBase64String(signature));
         }
-
     }
 }
 
